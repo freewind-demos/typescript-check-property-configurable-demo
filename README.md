3964913e8d5cb71fc2c6cd40bdece2e4467c8512
freewind <nowindlee@gmail.com>
2019-05-21 20:47:03 +0800

    demo

--- README.md
+++ README.md
@@ -1,18 +1,9 @@
-TypeScript Hello World Demo
-===========================
+TypeScript Check Property Configurable Demo
+===========================================
+
+测试某个property是否为configurable.
 
 ```
 npm install
 npm run demo
 ```
-
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
-
-```
-import some from 'some'
-```
-
--- hello.ts
+++ hello.ts
@@ -1,1 +1,21 @@
-console.log('Hello');
+const user: any = {}
+
+Object.defineProperty(user, 'name1', {
+  get() {
+    return "typescript1"
+  },
+  configurable: true
+})
+
+Object.defineProperty(user, 'name2', {
+  get() {
+    return "typescript2"
+  },
+  configurable: false
+})
+
+const name1 = Object.getOwnPropertyDescriptor(user, 'name1')
+console.log('name1:', name1!.configurable)
+
+const name2 = Object.getOwnPropertyDescriptor(user, 'name2')
+console.log('name2:', name2!.configurable)
